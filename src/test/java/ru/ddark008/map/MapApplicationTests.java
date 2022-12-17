55ba970ac1e6ddd1c5a3ce0c81e5ea7ea2135e58
Дмитрий А. Елисеев <i@ddark008.ru>
2022-12-17 14:43:25 +0300

    Init commit

---
+++ src/test/java/ru/ddark008/map/MapApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.ddark008.map;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
